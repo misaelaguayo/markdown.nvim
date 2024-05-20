408c20b6d7b9448b58487c83cb71bd5e2873ce03
Misael Aguayo <aguayo.misael@gmail.com>
2024-05-19 19:36:59 -0500

    Open vertical buff split

--- lua/markdown/init.lua
+++ lua/markdown/init.lua
@@ -4,6 +4,9 @@ function M.setup(opts)
     opts = opts or {}
 
     vim.keymap.set("n", "<leader>H", function ()
+        -- call markdown preview and open in vertical split
+        -- vim.fn.system('markdown preview blahu')
+        vim.cmd('vsp check')
         if opts.name then
             print("Hello " .. opts.name)
         else
