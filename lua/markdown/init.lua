57f7b91f48386a67b1482d090e33c0aaf5deb928
Misael Aguayo <aguayo.misael@gmail.com>
2024-05-20 21:36:33 -0500

    Plugin changes

--- lua/markdown/init.lua
+++ lua/markdown/init.lua
@@ -1,17 +1,21 @@
 local M = {}
 
+local utils = require("chafa")
+
 function M.setup(opts)
     opts = opts or {}
 
     vim.keymap.set("n", "<leader>H", function ()
         -- call markdown preview and open in vertical split
         -- vim.fn.system('markdown preview blahu')
-        vim.cmd('vsp check')
-        if opts.name then
-            print("Hello " .. opts.name)
-        else
-            print("Hello World")
-        end
+        local buf_id = 0
+        local buf_path = vim.api.nvim_buf_get_name(buf_id)
+        vim.cmd('vsp README')
+        -- if opts.name then
+        --     print("Hello " .. opts.name)
+        -- else
+        --     print("Hello World")
+        -- end
     end)
 end
 
